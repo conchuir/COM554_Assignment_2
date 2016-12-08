0cde32f0e5c8504ba301cd48ef1bc558eb48dbce
Connor O'Neill <connoroneill@Connors-MacBook-Pro.local>
2016-12-08 04:06:50 +0000

    Added search functionality and search help

--- js/site.js
+++ js/site.js
@@ -11,7 +11,7 @@ $( document ).ready(function() {
 
 
 // Universal news content loader
-// Tab id and url are passed in
+// Tab id and api request url are passed in
 function loadNews(url, tab_number) {
     $.getJSON(url, function(data) {
         $items = data['response']['results'];
@@ -22,7 +22,6 @@ function loadNews(url, tab_number) {
         //Add first news item to Jumbrotron box
         $("#j_button_"+ tab_number).attr("href", $items[0]['webUrl']);
         $("#j_title_"+ tab_number).append($items[0]['webTitle']);
-
         $("#jumbotron_"+ tab_number).css({"background":"linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255, 0.45)),url("+ $items[0]['fields']['thumbnail'] +") no-repeat","background-size":"100%"});
 
         //Add News Items
@@ -33,6 +32,7 @@ function loadNews(url, tab_number) {
                 $('#holder_'+tab_number).append('<div class="row_contain" id="row_contain_'+ tab_number +'_'+ j +'"></div>');
             }
 
+            // Build news item html
             var news_item = "\
                 <div id=\""+ $items[i]['id'] +"\" class=\"col-xs-6 col-sm-4\">\
                     <div class=\"panel panel-default\">\
@@ -54,31 +54,28 @@ function loadNews(url, tab_number) {
 }
 
 
+// Adds tab list items to nav bar
+function addNavItem(name, id)    {
+    var nav_item = "<li><a href=\"#tabs_"+ id +"\">"+ name +"</a></li>";
+    $("#nav_ul").append(nav_item);
+}
+
+// Adds tab items to page
+function addTab(id)   {
+    var tab = "\
+                <div id=\"tabs_"+ id +"\">\
+                    <div class=\"jumbotron\" id=\"jumbotron_"+ id +"\">\
+                        <h1 id=\"j_title_"+ id +"\"></h1>\
+                        <p><a href=\"\" id=\"j_button_"+ id +"\" class=\"btn btn-primary btn-lg\">View Story</a></p>\
+                    </div>\
+                    <div id='holder_"+ id +"'></div>\
+                </div>";
+    $("#tabs").append(tab);
+}
+
 // Build tabs and contents
 function buildContent(array)    {
     // Assuming multi-dimensional array of format: name, id, load point
-    var domain = "https://content.guardianapis.com/";
-    var api_key = "api-key=8b7ca0fc-3914-4473-9c07-e9b56781ce88";
-    var req_fields = "&show-fields=thumbnail";
-
-    // Adds list items to nav bar
-    function addNavItem(name, id)    {
-        var nav_item = "<li><a href=\"#tabs_"+ id +"\">"+ name +"</a></li>";
-        $("#nav_ul").append(nav_item);
-    }
-
-    function addTab(id)   {
-        var tab = "\
-                    <div id=\"tabs_"+ id +"\">\
-                        <div class=\"jumbotron\" id=\"jumbotron_"+ id +"\">\
-                            <h1 id=\"j_title_"+ id +"\"></h1>\
-                            <p><a href=\"\" id=\"j_button_"+ id +"\" class=\"btn btn-primary btn-lg\">View Story</a></p>\
-                        </div>\
-                        <div id='holder_"+ id +"'></div>\
-                    </div>";
-        $("#tabs").append(tab);
-    }
-
     // Ignore index 0 (headers) in outer array
     for (i=1; i<array.length; i++)  {
         var name = array[i][0];
@@ -86,25 +83,60 @@ function buildContent(array)    {
         var lp = array[i][2]; //lp = load point
         var ef = array[i][3]; //ef = extra fields
 
-        if (ef != "")   {
-            ef = ef + "&";
-        }
-        else {
-            ef = "?";
-        }
-
-        var url = "" + domain + lp + ef + api_key + req_fields;
+        var url = buildUrl(lp, ef);
 
         addNavItem(name, id);
         addTab(id);
         loadNews(url, id);
     }
 
-    // Adds a hidden ptab for search reults
+    // Adds a hidden tab for search reults
     addTab("99");
     $("#nav_ul").append("<li class=\"hidden\"><a href=\"#tabs_99\">Search</a></li>");
 
     $( function() {
         $( "#tabs" ).tabs();
     } );
+}
+
+// Build api request url
+function buildUrl(lp, ef)    {
+    var domain = "https://content.guardianapis.com/";
+    var api_key = "api-key=8b7ca0fc-3914-4473-9c07-e9b56781ce88";
+    var req_fields = "&show-fields=thumbnail";
+
+    if (ef != "")   {
+        ef = ef + "&";
+    }
+    else {
+        ef = "?";
+    }
+
+    var url = "" + domain + lp + ef + api_key + req_fields;
+    return url;
+}
+
+
+// Search bar javascript
+function search() {
+    // Pull query and build url
+    var query = $("#search_text").val().replace(/ /g, '%20');
+    var url = buildUrl("search", "?q=" + query);
+
+    //recreate results tab
+    $("#tabs_99").remove();
+    addTab("99");
+
+    //load results and switch tab
+    loadNews(url, "99");
+    $( "#tabs" ).tabs( "option", "active", content_array.length-1 );
+}
+
+//Show and hide help
+function help() {
+    $('#search_help').removeClass('hidden');
+}
+
+function hideHelp() {
+    $('#search_help').addClass('hidden');
 }
